053abfcb51f2c36ed46ae28319d85aa3b8b2c7d3
vinzmyko <vinzmykodelrosario@gmail.com>
2026-01-04 21:01:18 +0000

    feat(metrics): add Prometheus metrics endpoint

--- cmd/loadbalancer/main.go
+++ cmd/loadbalancer/main.go
@@ -10,13 +10,22 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
 	"github.com/vinzmyko/load-balancer/internal/config"
 )
 
 var (
-	counter      uint64
-	healthStatus map[int]bool
-	healthMutex  sync.RWMutex
+	// Backend
+	counter      uint64       // Which backend server to send to
+	healthStatus map[int]bool // All the backend server's health status
+	healthMutex  sync.RWMutex // Mutex for health related operations
+
+	// Prometheus metrics
+	requestsTotal   *prometheus.CounterVec
+	requestDuration *prometheus.HistogramVec
+	backendHealthy  *prometheus.GaugeVec
 )
 
 // Health checking function handler
@@ -53,8 +62,10 @@ func startHealthChecker(idx int, backendURL string) {
 			if healthStatus[idx] != isHealthy {
 				if isHealthy {
 					log.Printf("Backend %d (%s) is now HEALTHY", idx, backendURL)
+					backendHealthy.WithLabelValues(backendURL).Set(1)
 				} else {
 					log.Printf("Backend %d (%s) is now UNHEALTHY", idx, backendURL)
+					backendHealthy.WithLabelValues(backendURL).Set(0)
 				}
 				healthStatus[idx] = isHealthy
 			}
@@ -64,10 +75,21 @@ func startHealthChecker(idx int, backendURL string) {
 }
 
 // Forwards requests to backends
-func proxyHandler(proxies []*httputil.ReverseProxy) http.HandlerFunc {
+func proxyHandler(proxies []*httputil.ReverseProxy, backends []config.BackendConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
 		backend := selectBackend(proxies)
+		backendURL := backends[backend].URL
+
+		// Increment backend request counter
+		requestsTotal.WithLabelValues(backendURL).Inc()
+
+		// Forward request to backend
 		proxies[backend].ServeHTTP(w, r)
+
+		duration := time.Since(start).Seconds()
+		requestDuration.WithLabelValues(backendURL).Observe(duration) // Add measurement to histogram
 	}
 }
 
@@ -76,6 +98,35 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
+
+	requestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "loadbalancer_requests_total",
+			Help: "Total number of requests forwarded to each backend",
+		},
+		[]string{"backend"}, // Label
+	)
+
+	requestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "loadbalancer_request_duration_seconds",
+			Help:    "Request duration in seconds",
+			Buckets: prometheus.DefBuckets, // Default ranges e.g. [5ms, 10ms ,25ms ,50ms,  100ms, etc.]
+		},
+		[]string{"backend"},
+	)
+
+	backendHealthy = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "loadbalancer_backend_healthy",
+			Help: "Backend health status (1 = healthy, 0 = unhealthy)",
+		},
+		[]string{"backends"},
+	)
+
+	prometheus.MustRegister(requestsTotal)
+	prometheus.MustRegister(requestDuration)
+	prometheus.MustRegister(backendHealthy)
 
 	var proxies []*httputil.ReverseProxy
 	for _, backend := range cfg.Backends {
@@ -91,8 +142,19 @@ func main() {
 		startHealthChecker(i, backend.URL)
 	}
 
+	metricsMux := http.NewServeMux()
+	metricsMux.Handle("/metrics", promhttp.Handler())
+
+	go func() {
+		metricsAddr := ":9090"
+		log.Printf("Starting metrics server on %s", metricsAddr)
+		if err := http.ListenAndServe(metricsAddr, metricsMux); err != nil {
+			log.Fatalf("Metrics server failed: %v", err)
+		}
+	}()
+
 	http.HandleFunc("/health", healthHandler)
-	http.HandleFunc("/", proxyHandler(proxies))
+	http.HandleFunc("/", proxyHandler(proxies, cfg.Backends))
 
 	addr := fmt.Sprintf(":%d", cfg.Server.Port)
 	log.Printf("Starting load balancer on %s", addr)
